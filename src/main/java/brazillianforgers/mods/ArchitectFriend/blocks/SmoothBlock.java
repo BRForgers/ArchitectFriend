f80e0ae89fe71dfa03fb8d82f9c14bc23c673f9e
Adrian Todt Monteiro da Silva <adriantodt@outlook.com>
2015-11-08 06:57:11 -0200

    Time for Modding...

--- src/main/java/brazillianforgers/mods/ArchitectFriend/blocks/SmoothBlock.java
+++ src/main/java/brazillianforgers/mods/ArchitectFriend/blocks/SmoothBlock.java
@@ -1,14 +1,26 @@
 package brazillianforgers.mods.ArchitectFriend.blocks;
 
+import java.util.List;
+
+import com.sun.beans.editors.IntegerEditor;
+
 import brazillianforgers.lib.RecipeHelper.ICraftable;
 import brazillianforgers.lib.RecipeHelper.Recipe;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 
 public class SmoothBlock extends Block implements ICraftable {
-
-	protected SmoothBlock(Material p_i45394_1_) {
-		super(p_i45394_1_);
+	protected IIcon[] icons;
+	
+	public SmoothBlock() {
+		super(Material.rock);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -17,5 +29,35 @@ public class SmoothBlock extends Block implements ICraftable {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/**
+     * Gets the block's texture. Args: side, meta
+     */
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int metadata)
+    {
+        return icons[metadata];
+    }
+
+    /**
+     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+     */
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item item, CreativeTabs tab, List list)
+    {
+    	for (int i = 0; i < 16; ++i)
+        {
+    		list.add(new ItemStack(item, 1, 0));
+        }
+    }
+    
+	@SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister register)
+    {
+        for (int i = 0; i < 16; ++i)
+        {
+            this.icons[i] = register.registerIcon("architectfriend:Smooth_" + Integer.toString(i));
+        }
+    }
 
 }
--- src/main/java/brazillianforgers/mods/ArchitectFriend/manager/BlockManager.java
+++ src/main/java/brazillianforgers/mods/ArchitectFriend/manager/BlockManager.java
@@ -1,14 +1,19 @@
 package brazillianforgers.mods.ArchitectFriend.manager;
 
 import net.minecraft.block.Block;
+import brazillianforgers.lib.RecipeHelper.ICraftable;
+import brazillianforgers.mods.ArchitectFriend.blocks.*;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class BlockManager {
-	public static Block SmoothBlock;
-	public static Block SmoothBricks;
-	public static Block SmoothBrickies;
+	public static Block smoothBlock;
+	public static Block smoothBricks;
+	public static Block smoothBrickies;
 	
 	public static void init()
 	{
-		
+		smoothBlock = new SmoothBlock();
+		RecipeHandler.addToRecipes((ICraftable) smoothBlock);
+		GameRegistry.registerBlock(smoothBlock, "smoothBlock");
 	}
 }
